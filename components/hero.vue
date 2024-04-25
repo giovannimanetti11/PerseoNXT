9dde93305a264749dcfd3fee767313c9ea25a639
giovannimanetti11 <giovanni@perseodesign.com>
2024-04-25 19:12:21 +0000

    Add hero component

---
+++ components/hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="hero-section py-10 w-11/12 h-80 m-auto m-0 rounded-2xl">
+    <div class="container mx-auto px-4 mt-24">
+      <h2 class="text-5xl font-bold text-center text-black">Erbe Aromatiche e Medicinali</h2>
+      <p class="text-center text-lg mt-2 text-black">
+        Le schede monografiche delle 61 erbe presenti su WikiHerbalist illustrano erbe officinali e erbe aromatiche.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+.hero-section {
+    background: linear-gradient(153deg, #0da6a475 0%, #DFEDFD 100%);
+}
+
+</style>
