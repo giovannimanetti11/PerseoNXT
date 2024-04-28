1bf92335620d0c876f67c38cbc03874db6335f9d
giovannimanetti11 <giovanni@perseodesign.com>
2024-04-28 00:21:31 +0000

    Add search field component

---
+++ components/searchfield.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="relative w-full max-w-lg">
+      <div class="rounded-2xl h-14 bg-white flex items-center border border-sky-500 overflow-hidden">
+        <Icon name="heroicons:magnifying-glass-16-solid" class="ml-5 text-sky-500 text-2xl" />
+        <input
+          type="search"
+          placeholder="Cerca in Wikiherbalist"
+          class="w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none"
+        />
+      </div>
+    </div>
+</template>
+
+
+
+
+<script setup>
+  import { Icon } from '#components'
+</script>
+
+<style scoped>
+</style>
