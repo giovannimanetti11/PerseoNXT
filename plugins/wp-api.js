1b3efe3ce3d20e5a71abcb1c2fb437c0aef42c25
giovannimanetti11 <giovanni@perseodesign.com>
2024-04-29 20:10:05 +0000

    Add lru-cache

--- plugins/wp-api.js
+++ plugins/wp-api.js
@@ -1,14 +1,31 @@
-import { apiConfig } from '../config.js';
+import LRU from 'lru-cache';
+import { apiConfig } from '../config.js'; 
+
+// Create a new cache object
+const apiCache = new LRU({
+  max: 100, 
+  maxAge: 1000 * 60 * 15 // Cache for 15 minutes
+});
 
 export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.provide('fetchWP', async (endpoint) => {
+    const cacheKey = endpoint;
+
+    // Check if the data is in cache
+    if (apiCache.has(cacheKey)) {
+      return apiCache.get(cacheKey);
+    }
+
     try {
       const response = await $fetch(`${apiConfig.baseUrl}${endpoint}`, {
         method: 'GET',
         headers: {
-          'Authorization': `Bearer ${apiConfig.jwtToken}`
+          'Authorization': `Bearer ${apiConfig.jwtToken}` 
         }
       });
+
+      // Store the response in cache
+      apiCache.set(cacheKey, response);
       return response;
     } catch (error) {
       console.error('Error fetching data from WordPress:', error);
