9f4276584b89e9651c4dfcf70ea64fc72fa73908
giovannimanetti11 <giovanni@perseodesign.com>
2024-05-02 22:29:51 +0000

    Style blog cards

--- components/section-blog.vue
+++ components/section-blog.vue
@@ -1,36 +1,35 @@
 <template>
-    <section class="py-10 mt-6 w-11/12 mx-auto rounded-2xl section-blog flex"> 
-        <!-- Left column -->
-        <div class="w-2/5 p-8 ml-6">
-            <h3 class="text-3xl font-bold mb-4">Blog</h3>
-            <p class="mb-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit nunc et velit rhoncus, sed fermentum risus feugiat. Aliquam at turpis dictum, hendrerit metus eget, lobortis est.
-            </p>
-            <button class="bg-transparent text-blu py-2 px-4 border border-blu rounded-xl hover:text-verde hover:border-verde">
-            Esplora gli articoli
-            </button>
-        </div>
-        <!-- Right column -->
-        <div class="w-3/5 flex flex-wrap justify-around">
-            <div class="w-full flex flex-wrap justify-around">
-                <div v-for="post in posts" :key="post.uri" @click="toggleActive(post.uri)" :class="['w-7/12 shadow flex flex-row rounded-2xl shadow-lg m-4 cursor-pointer transition duration-300 ease-in-out transform', post.uri === activePost ? 'bg-blu text-white scale-110' : 'bg-white text-black hover:bg-blu hover:text-white']">
-                    <div class="px-6 py-4 flex flex-col justify-between items-start h-full w-2/4">
-                        <div class="font-bold text-xl mb-auto">{{ post.title }}</div>
-                        <div>{{ post.authorName }}</div>
-                        <div>{{ post.date }}</div>
-                    </div>
-                    <div class="flex-col w-2/4 m-auto text-center">
-                        <img :src="post.featuredImage" :alt="post.altText" class="rounded-2xl w-11/12 mt-4 m-auto h-auto object-cover">
-                        <button class="py-4 bg-verde text-white hover:bg-blu w-11/12 rounded-xl mt-4 mb-4">
-                            Leggi di più  →
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+  <section class="py-10 mt-6 w-11/12 mx-auto rounded-2xl section-blog flex" :style="{ height: activeHeight }"> 
+      <!-- Left column -->
+      <div class="w-2/5 p-8 ml-6">
+          <h3 class="text-3xl font-bold mb-4">Blog</h3>
+          <p class="mb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit nunc et velit rhoncus, sed fermentum risus feugiat. Aliquam at turpis dictum, hendrerit metus eget, lobortis est.
+          </p>
+          <button class="bg-transparent text-blu py-2 px-4 border border-blu rounded-xl hover:text-verde hover:border-verde">
+          Esplora gli articoli
+          </button>
+      </div>
+      <!-- Right column -->
+      <div class="w-3/5 flex flex-wrap justify-around">
+          <div class="w-full flex flex-wrap justify-around">
+              <div v-for="post in posts" :key="post.uri" @click="toggleActive(post.uri)" :class="['blog-card w-7/12 shadow flex flex-row rounded-2xl shadow-lg m-4 cursor-pointer transition duration-300 ease-in-out transform', post.uri === activePost ? 'active bg-celeste text-white scale-110 z-10' : 'bg-white text-black hover:bg-celeste hover:text-white']">
+                  <div class="px-6 py-4 flex flex-col justify-between items-start h-full w-2/4">
+                      <div class="font-bold text-xl mb-auto">{{ post.title }}</div>
+                      <div class="text-gray-500 blog-details">{{ post.authorName }}</div>
+                      <div class="text-gray-500 blog-details">{{ post.date }}</div>
+                  </div>
+                  <div class="flex-col w-2/4 m-auto text-center">
+                      <img :src="post.featuredImage" :alt="post.altText" class="rounded-2xl w-11/12 mt-4 m-auto h-auto object-cover">
+                      <button class="blog-card-button py-4 bg-verde text-white hover:bg-blu w-11/12 rounded-xl mt-4 mb-4">
+                          Leggi di più  →
+                      </button>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
 </template>
-  
 
 
 <script setup>
@@ -41,80 +40,92 @@ import gql from 'graphql-tag';
 // Define ref for posts
 const posts = ref([]);
 
+const activePost = ref(null);
+
 // GraphQL query for blog posts
 const BLOG_POSTS_QUERY = gql`
-  query BlogPosts {
-    blogPosts(first: 3, where: {orderby: {field: DATE, order: DESC}}) {
-      nodes {
-        authorName
-        featuredImage {
-          node {
-            altText
-            sourceUrl
-          }
+query BlogPosts {
+  blogPosts(first: 3, where: {orderby: {field: DATE, order: DESC}}) {
+    nodes {
+      authorName
+      featuredImage {
+        node {
+          altText
+          sourceUrl
         }
-        title
-        uri
-        date
       }
+      title
+      uri
+      date
     }
   }
+}
 `;
 
 // Use Apollo Composable to fetch blog posts
-const { result, loading, error } = useQuery(BLOG_POSTS_QUERY);
-
-// Watch for changes in loading and error
-watch(loading, (newLoading) => {
-  console.log('Stato di caricamento:', newLoading);
-});
-
-watch(error, (newError) => {
-  if (newError) {
-    console.error('Errore durante il fetch dei dati:', newError);
-  }
-});
+const { result } = useQuery(BLOG_POSTS_QUERY);
 
 // Watch for changes in result
 watch(result, (newValue) => {
-  if (newValue?.blogPosts?.nodes) {
-    posts.value = newValue.blogPosts.nodes.map(post => ({
-      authorName: post.authorName,
-      featuredImage: post.featuredImage.node.sourceUrl,
-      altText: post.featuredImage.node.altText,
-      title: post.title,
-      uri: post.uri,
-      date: new Date(post.date).toLocaleDateString()
+if (newValue?.blogPosts?.nodes) {
+  posts.value = newValue.blogPosts.nodes.map(post => ({
+    authorName: post.authorName,
+    featuredImage: post.featuredImage.node.sourceUrl,
+    altText: post.featuredImage.node.altText,
+    title: post.title,
+    uri: post.uri,
+    date: new Date(post.date).toLocaleDateString('it-IT', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+      })
     }));
-  }
+    activePost.value = posts.value[0]?.uri;
+}
 });
 
-// Populate posts on component mount
-onMounted(() => {
-  console.log('Risultato completo:', result.value);
-  if (error.value) {
-    console.error('Errore GraphQL:', error.value);
-  }
-  if (loading.value) {
-    console.log('Caricamento in corso...');
-  }
-});
+const toggleActive = (uri) => {
+  activePost.value = uri;
+};
+
+
 </script>
 
 
-
-
-
-
-
 <style scoped>
-.section-blog {
-  background-image: linear-gradient(180deg, rgba(245,245,245,1) 0%, rgba(224,237,253,1) 82%);
-}
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to {
-  opacity: 0;
-}
+  .section-blog {
+    background-image: linear-gradient(180deg, rgba(245,245,245,1) 0%, rgba(224,237,253,1) 82%);
+    max-height: 625px;
+  }
+
+  .blog-card:hover .blog-details, .blog-card.active .blog-details {
+    color: #fff;
+  }
+
+  .blog-card:hover .blog-card-button:hover, .blog-card:hover .blog-card-button, .blog-card.active .blog-card-button {
+    background-color: #fff;
+    color: #5E9EF4;
+  }
+
+  .blog-card:first-child {
+    transform: translateX(40px);
+  }
+
+  .blog-card:nth-child(2) {
+    transform: translate(-80px, -100px);
+  }
+
+  .blog-card:last-child {
+    transform: translate(90px, -270px);
+  }
+
+  .blog-card {
+    z-index: 0;
+    transition: transform 0.3s ease-in-out, z-index 0s;
+  }
+
+  .blog-card.active {
+    z-index: 10;
+  }
+
 </style>
