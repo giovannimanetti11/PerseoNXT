d1fc873ec40398caa8ba091c1d2423d33c76f910
giovannimanetti11 <giovanni@perseodesign.com>
2024-05-02 11:10:40 +0000

    Add blog section component

---
+++ components/section-blog.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="py-10 mt-6 w-11/12 mx-auto rounded-2xl section-blog"> 
+        <!-- Left col -->
+        <div class="w-2/5 bg-gradient-to-b p-8 ml-6">
+            <h3 class="text-3xl font-bold mb-4">Blog</h3>
+            <p class="mb-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit nunc et velit rhoncus, sed fermentum risus feugiat. Aliquam at turpis dictum, hendrerit metus eget, lobortis est.
+            </p>
+            <button class="bg-transparent text-verde py-2 px-4 border border-verde rounded-xl hover:text-blu hover:border-blu">
+            Esplora gli articoli
+            </button>
+        </div>
+        <!-- Right col -->
+        <div class="w-3/5">
+        </div>
+    </section>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.section-blog {
+  background-image: linear-gradient(180deg, rgba(245,245,245,1) 0%, rgba(224,237,253,1) 82%);
+}
+</style>
