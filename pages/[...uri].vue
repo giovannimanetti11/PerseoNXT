dd3bd478f5385af34d266b3c07b482279826a91c
giovannimanetti11 <giovanni@perseodesign.com>
2024-05-05 09:15:41 +0000

    Add graphQL query with all content

--- pages/[...uri].vue
+++ pages/[...uri].vue
@@ -3,10 +3,21 @@
     <div class="mt-40 container mx-auto px-4">
       <h1 class="text-5xl">{{ post.title }}</h1>
       <h2 class="text-2xl">{{ post.nomeScientifico }}</h2>
+      <div v-html="post.excerpt"></div>
+      <p><span class="font-bold">Parti usate:</span> {{ post.partiUsate }}</p>
+      <p><span class="font-bold">Nome comune:</span> {{ post.nomeComune }}</p>
+      <p><span class="font-bold">Tossica:</span> {{ post.tossica ? 'Sì' : 'No' }}</p>
+      <p><span class="font-bold">Costituenti principali:</span> {{ post.costituenti }}</p>
+      <p><span class="font-bold">Autore:</span> {{ post.authorName }}</p>
+      <p><span class="font-bold">Data di pubblicazione:</span> {{ new Date(post.date).toLocaleDateString() }}</p>
+      <p><span class="font-bold">Ultima modifica:</span> {{ new Date(post.modified).toLocaleDateString() }}</p>
+      <div v-html="post.content"></div>
     </div>
   </section>
-
 </template>
+
+
+
 
 <script setup>
 import { ref, onMounted, watch } from 'vue';
@@ -23,15 +34,38 @@ const post = ref(null);
 const FETCH_POST_BY_URI = gql`
   query FetchPostByUri($uri: String!) {
     postBy(uri: $uri) {
+      id
       title
       nomeScientifico
+      partiUsate
+      nomeComune
+      tossica
+      excerpt
+      costituenti
+      authorName
+      date
+      modified
+      content
+      featuredImage {
+        node {
+          altText
+          caption
+          sourceUrl
+        }
+      }
+      additionalImages {
+        altText
+        caption
+        url
+      }
     }
   }
 `;
 
+
+
 async function fetchPost() {
   const uri = route.params.uri instanceof Array ? route.params.uri[0] : route.params.uri;
-
   const { result, loading, error } = useQuery(FETCH_POST_BY_URI, { uri });
 
   watch(loading, (newLoading) => {
@@ -49,6 +83,7 @@ async function fetchPost() {
 
 onMounted(fetchPost);
 </script>
+
 
 
 
