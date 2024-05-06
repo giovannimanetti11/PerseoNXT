5855e53a690c782870fb8839486ef49fed91ec01
giovannimanetti11 <giovanni@perseodesign.com>
2024-05-06 19:45:53 +0000

    Split components and add props to PostInfo and Slideshow

--- pages/[...uri].vue
+++ pages/[...uri].vue
@@ -1,35 +1,42 @@
 <template>
-  <section class="post-detail-section py-10 w-11/12 mx-auto rounded-2xl" v-if="post">
-    <div class="mt-40 container mx-auto px-4">
-      <h1 class="text-5xl">{{ post.title }}</h1>
-      <h2 class="text-2xl">{{ post.nomeScientifico }}</h2>
-      <div v-html="post.excerpt"></div>
-      <p><span class="font-bold">Parti usate:</span> {{ post.partiUsate }}</p>
-      <p><span class="font-bold">Nome comune:</span> {{ post.nomeComune }}</p>
-      <p><span class="font-bold">Tossica:</span> {{ post.tossica ? 'Sì' : 'No' }}</p>
-      <p><span class="font-bold">Costituenti principali:</span> {{ post.costituenti }}</p>
-      <p><span class="font-bold">Autore:</span> {{ post.authorName }}</p>
-      <p><span class="font-bold">Data di pubblicazione:</span> {{ new Date(post.date).toLocaleDateString() }}</p>
-      <p><span class="font-bold">Ultima modifica:</span> {{ new Date(post.modified).toLocaleDateString() }}</p>
-      <div v-html="post.content"></div>
+  <section class="post-section flex flex-row py-10 w-10/12 mx-auto rounded-2xl" v-if="post.data">
+    <div class="mt-40 container mx-auto w-2/5 px-4">
+      <PostInfo 
+        :title="post.data.title"
+        :nomeScientifico="post.data.nomeScientifico"
+        :publishDate="post.data.date"
+        :updateDate="post.data.modified"
+        :content="post.data.content"
+      />
+
+    </div>
+    <div class="w-3/5">
+      <div v-if="post.data.featuredImage && post.additionalImagesFiltered.length > 0">
+        <Slideshow 
+          :featured-image="post.data.featuredImage"
+          :additional-images="post.additionalImagesFiltered"
+        />
+      </div>
     </div>
   </section>
 </template>
 
-
-
-
 <script setup>
-import { ref, onMounted, watch } from 'vue';
+import { ref, reactive, onMounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { useNuxtApp } from '#app';
 import { useQuery } from '@vue/apollo-composable';
 import gql from 'graphql-tag';
+import Slideshow from '@/components/posts/slideshow.vue';
+import PostInfo from '@/components/posts/postinfo.vue';
 
 const route = useRoute();
 const { apollo } = useNuxtApp();
 
-const post = ref(null);
+const post = reactive({
+  data: null,
+  additionalImagesFiltered: []
+});
 
 const FETCH_POST_BY_URI = gql`
   query FetchPostByUri($uri: String!) {
@@ -62,15 +69,16 @@ const FETCH_POST_BY_URI = gql`
   }
 `;
 
-
-
 async function fetchPost() {
   const uri = route.params.uri instanceof Array ? route.params.uri[0] : route.params.uri;
   const { result, loading, error } = useQuery(FETCH_POST_BY_URI, { uri });
 
   watch(loading, (newLoading) => {
     if (!newLoading && !error.value) {
-      post.value = result.value?.postBy;
+      post.data = result.value?.postBy;
+      if (post.data && post.data.additionalImages) {
+        post.additionalImagesFiltered = post.data.additionalImages.filter(img => img && img.url);
+      }
     }
   });
 
@@ -84,11 +92,8 @@ async function fetchPost() {
 onMounted(fetchPost);
 </script>
 
-
-
-
 <style scoped>
-.post-detail-section {
+.post-section {
   background: rgb(245,245,245);
   background: linear-gradient(180deg, rgba(224,237,253,1) 0%, rgba(245,245,245,1) 100%);
 }
