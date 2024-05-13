b3209da903da061331657f571a53ba86d46677af
giovannimanetti11 <giovanni@perseodesign.com>
2024-05-13 18:45:08 +0000

    Add footer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,13 @@
   <div>
     <Header></Header>
     <NuxtPage />
+    <Footer></Footer>
   </div>
 </template>
 
 <script setup>
 import Header from '~/components/header.vue';
+import Footer from '~/components/footer.vue';
 </script>
 
 <style scoped>
