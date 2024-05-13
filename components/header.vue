eed077747bd08907fdb425f0e6344d1a8b6820a7
giovannimanetti11 <giovanni@perseodesign.com>
2024-05-13 22:10:53 +0000

    Switch back to <img> because of lack of svg support for <NuxtImg>

--- components/header.vue
+++ components/header.vue
@@ -3,7 +3,8 @@
     <div :class="['flex justify-between items-center bg-white py-4 px-6 shadow-lg rounded-2xl', {'radius-bottom-none': menuOpen || mailingListMenuOpen}]">
       <div class="flex items-center">
         <a href="https://wikinutritionals.com">
-          <NuxtImg src="/media/logo_wikiherbalist.svg" alt="WikiHerbalist Logo" class="h-8 mr-2" />
+          <img src="/media/logo_wikiherbalist.svg" alt="WikiHerbalist Logo" class="h-8 mr-2" loading="lazy">
+
         </a>
       </div>
 
