f7fbb57b994a1996a96c7672a2f43eced3de279a
giovannimanetti11 <giovanni@perseodesign.com>
2024-05-17 15:51:34 +0000

    Add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import pluginVue from 'eslint-plugin-vue';
+import pluginUnicorn from 'eslint-plugin-unicorn';
+import pluginTypeScript from '@typescript-eslint/eslint-plugin';
+import parserTypeScript from '@typescript-eslint/parser';
+import vueParser from 'vue-eslint-parser';
+
+export default [
+  {
+    files: ['**/*.{js,vue,ts}'],
+    ignores: ['node_modules/**', '.output/**', '.nuxt/**', 'dist/**'],
+    plugins: {
+      vue: pluginVue,
+      unicorn: pluginUnicorn,
+      '@typescript-eslint': pluginTypeScript,
+    },
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: parserTypeScript,
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+        extraFileExtensions: ['.vue'],
+      },
+    },
+    rules: {
+      'vue/no-unused-vars': 'error',
+      'vue/require-prop-types': 'error',
+      'unicorn/prefer-at': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'unicorn/no-array-reduce': 'error',
+    },
+  },
+];
