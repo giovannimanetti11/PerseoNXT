ba31e96b05ee84b4808a9441c7450c1763ed485c
giovannimanetti11 <giovanni@perseodesign.com>
2024-05-18 08:57:00 +0000

    Add lightbox, improved code and comments

--- components/posts/slideshow.vue
+++ components/posts/slideshow.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="mt-24 flex flex-col float-right">
-    <div v-if="currentImage" class="relative h-auto w-96 float-right">
-      <NuxtImg :src="currentImage" class="m-auto h-60 w-auto border rounded-2xl transition-all duration-300 ease-in-out" :alt="currentAltText" />
+    <!-- Image section that triggers lightbox on click -->
+    <div v-show="currentImage" class="relative h-auto w-96 float-right">
+      <NuxtImg
+        :src="currentImage"
+        class="m-auto h-60 w-auto border rounded-2xl transition-all duration-300 ease-in-out"
+        :alt="currentAltText"
+        @click="openLightbox"
+      />
     </div>
 
-    <div v-if="additionalImages.length > 0" class="w-full mt-1.5 flex flex-wrap justify-center">
-      <NuxtImg 
-        v-if="featuredImage && featuredImage.node && featuredImage.node.sourceUrl"
-        :src="featuredImage.node.sourceUrl" 
-        :class="thumbnailClass(featuredImage.node.sourceUrl)"
-        @click="setCurrentImage(featuredImage.node.sourceUrl, featuredImage.node.altText)"
-        @mouseover="setCurrentImage(featuredImage.node.sourceUrl, featuredImage.node.altText)"
-        :alt="featuredImage.node.altText"
-      />
-      <!-- Filter and display only images with valid URLs -->
-      <div v-for="(image, index) in additionalImages.filter(img => img.url)" :key="index" class="h-16">
+    <!-- Thumbnails of additional images -->
+    <div v-show="additionalImages.length > 0" class="w-full mt-1.5 flex flex-wrap justify-center">
+      <div v-for="(image, index) in validAdditionalImages" :key="index" class="h-16">
         <NuxtImg 
-          :src="image.url" 
+          :src="image.url"
           :class="thumbnailClass(image.url)"
           @click="setCurrentImage(image.url, image.altText)"
           @mouseover="setCurrentImage(image.url, image.altText)"
@@ -24,33 +22,60 @@
         />
       </div>
     </div>
+
+    <!-- Lightbox component from vue-easy-lightbox -->
+    <vue-easy-lightbox
+      :visible="lightboxVisible"
+      :imgs="allImages"
+      :index="currentImageIndex"
+      @hide="closeLightbox"
+    ></vue-easy-lightbox>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import { ref, computed } from 'vue';
+import VueEasyLightbox from 'vue-easy-lightbox';
 
 const props = defineProps({
   featuredImage: Object,
   additionalImages: Array
 });
 
-const currentImage = ref(props.featuredImage && props.featuredImage.node ? props.featuredImage.node.sourceUrl : null);
-const currentAltText = ref(props.featuredImage && props.featuredImage.node ? props.featuredImage.node.altText : '');
 
-function setCurrentImage(imageUrl, altText) {
-  if (imageUrl) {
-    currentImage.value = imageUrl;
-    currentAltText.value = altText;
-  }
-}
+const currentImage = ref(props.featuredImage?.node?.sourceUrl || null);
+const currentAltText = ref(props.featuredImage?.node?.altText || '');
 
-const validAdditionalImages = computed(() => {
-  return props.additionalImages.filter(image => image && image.url);
-});
+const lightboxVisible = ref(false);
 
+// Compute array of all image URLs for the lightbox
+const allImages = computed(() => [props.featuredImage.node.sourceUrl, ...props.additionalImages.map(img => img.url)]);
+
+// Compute current index of the displayed image within the lightbox
+const currentImageIndex = computed(() => allImages.value.indexOf(currentImage.value));
+
+// Set current image
+const setCurrentImage = (imageUrl, altText) => {
+  currentImage.value = imageUrl;
+  currentAltText.value = altText;
+};
+
+// Open the lightbox
+const openLightbox = () => {
+  lightboxVisible.value = true;
+};
+
+// Close the lightbox
+const closeLightbox = () => {
+  lightboxVisible.value = false;
+};
+
+// Filter valid additional images (prevent blank images if url is null)
+const validAdditionalImages = computed(() => props.additionalImages.filter(image => image?.url));
+
+// Thumbnail class based on current image
 const thumbnailClass = (imageUrl) => [
   'cursor-pointer rounded-2xl transition-all duration-300 ease-in-out mx-0.5',
-  currentImage.value === imageUrl ? 'w-16 h-16 border-3 border-blu' : 'w-14 h-14 border-3 border-celeste'
+  currentImage.value === imageUrl ? 'w-16 h-16 border-3 border-blue' : 'w-14 h-14 border-3 border-light-blue'
 ];
 </script>
