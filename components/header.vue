9ef185092f39923d37be6a56134f09805361fe40
giovannimanetti11 <giovanni@perseodesign.com>
2024-06-10 11:30:41 +0000

    refactor: update menu links and button styles
    - Change URL of 'Blog' link to /blog
    - Update styles for Mailing List button
    - Add hover effect for icons in mega menu buttons
    - Create separated component for MailingList

--- components/header.vue
+++ components/header.vue
@@ -3,7 +3,7 @@
     <div :class="['flex justify-between items-center bg-white py-4 px-6 shadow-lg rounded-2xl', {'radius-bottom-none': menuOpen || mailingListMenuOpen}]">
       <div class="flex items-center">
         <a href="https://wikinutritionals.com">
-          <img src="/media/logo_wikiherbalist.svg" alt="WikiHerbalist Logo" class="h-8 mr-2" loading="lazy">
+          <img src="/media/logo_wikiherbalist.svg" alt="Wikiherbalist Logo" class="h-8 mr-2" loading="lazy">
         </a>
       </div>
 
@@ -13,16 +13,16 @@
           <a href="#" class="no-underline py-2 px-2">About</a>
           <a href="#" class="no-underline py-2 px-2">Piante medicinali</a>
           <a href="/glossario" class="no-underline py-2 px-2">Glossario</a>
-          <a href="#" class="no-underline py-2 px-2">Blog</a>
+          <a href="/blog" class="no-underline py-2 px-2">Blog</a>
         </nav>
       </div>
 
       <div class="flex items-center">
-        <button class="buttonMailing mr-4" @click="toggleMailingListMenu">Mailing List</button>
+        <button class="buttonMailing py-1 px-5 mr-4 bg-white text-verde border border-verde hover:text-blu hover:border-blu rounded-md" @click="toggleMailingListMenu">Mailing List</button>
         <div class="hamburger-menu cursor-pointer flex flex-row items-center gap-1.5 ml-2" :class="{ 'active': menuOpen }" @click="toggleMenu">
-          <div class="bar rounded-lg"></div>
-          <div class="bar rounded-lg"></div>
-          <div class="bar rounded-lg"></div>
+          <div class="bar w-1.5 h-7 m-0.5 transition-all rounded-lg" :class="{ 'transform -translate-x-0.5 rotate-30': menuOpen }"></div>
+          <div class="bar w-1.5 h-5 m-0.5 transition-all rounded-lg" :class="{ 'mt-1': menuOpen }"></div>
+          <div class="bar w-1.5 h-7 m-0.5 transition-all rounded-lg" :class="{ 'transform translate-x-0.5 rotate-30': menuOpen }"></div>
         </div>
       </div>
     </div>
@@ -50,35 +50,35 @@
           <div class="w-2/3 flex justify-start items-center p-2">
             <div class="w-full grid grid-cols-2 gap-4">
               <div class="p-2">
-                <button class="mega-menu-button relative text-white py-4 px-4 bg-celeste rounded-lg w-full h-16 transition-all duration-500 ease-in-out hover:bg-verde before:absolute before:inset-0 before:bg-verde before:border before:rounded-lg before:transition-width before:duration-300 before:ease-linear before:origin-left before:scale-x-0 hover:before:scale-x-100">
-                  <div class="absolute left-2 top-1/2 w-12 h-12 -translate-y-1/2 bg-white p-2 rounded-2xl border border-transparent">
-                    <Icon name="mdi:event" class="inner-icon text-celeste text-3xl" />
+                <button class="mega-menu-button relative text-white py-4 px-4 bg-celeste rounded-lg w-full h-16 transition-all duration-500 ease-in-out group hover:bg-verde before:absolute before:inset-0 before:bg-verde before:border before:rounded-lg before:transition-all before:duration-300 before:ease-linear before:origin-left before:scale-x-0 hover:before:scale-x-100">
+                  <div class="absolute left-2 top-1/2 w-12 h-12 -translate-y-1/2 bg-white p-2 rounded-2xl border border-transparent flex justify-center items-center">
+                    <Icon name="mdi:event" class="inner-icon text-celeste text-3xl group-hover:text-verde" />
                   </div>
-                  <span class="absolute text-white top-1/2 -translate-y-1/2">Prossimi eventi</span>
+                  <span class="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Prossimi eventi</span>
                 </button>
               </div>
               <div class="p-2">
-                <button class="mega-menu-button relative text-white py-4 px-4 bg-celeste rounded-lg w-full h-16 transition-all duration-500 ease-in-out hover:bg-verde before:absolute before:inset-0 before:bg-verde before:border before:rounded-lg before:transition-width before:duration-300 before:ease-linear before:origin-left before:scale-x-0 hover:before:scale-x-100">
-                  <div class="absolute left-2 top-1/2 w-12 h-12 -translate-y-1/2 bg-white p-2 rounded-2xl border border-transparent">
-                    <Icon name="mdi:contact-mail" class="inner-icon text-celeste text-3xl" />
+                <button class="mega-menu-button relative text-white py-4 px-4 bg-celeste rounded-lg w-full h-16 transition-all duration-500 ease-in-out group hover:bg-verde before:absolute before:inset-0 before:bg-verde before:border before:rounded-lg before:transition-all before:duration-300 before:ease-linear before:origin-left before:scale-x-0 hover:before:scale-x-100">
+                  <div class="absolute left-2 top-1/2 w-12 h-12 -translate-y-1/2 bg-white p-2 rounded-2xl border border-transparent flex justify-center items-center">
+                    <Icon name="mdi:contact-mail" class="inner-icon text-celeste text-3xl group-hover:text-verde" />
                   </div>
-                  <span class="absolute text-white top-1/2 -translate-y-1/2">Contattaci</span>
+                  <span class="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Contattaci</span>
                 </button>
               </div>
               <div class="p-2">
-                <button class="mega-menu-button relative text-white py-4 px-4 bg-celeste rounded-lg w-full h-16 transition-all duration-500 ease-in-out hover:bg-verde before:absolute before:inset-0 before:bg-verde before:border before:rounded-lg before:transition-width before:duration-300 before:ease-linear before:origin-left before:scale-x-0 hover:before:scale-x-100">
-                  <div class="absolute left-2 top-1/2 w-12 h-12 -translate-y-1/2 bg-white p-2 rounded-2xl border border-transparent">
-                    <Icon name="mdi:book-open-variant" class="inner-icon text-celeste text-3xl" />
+                <button class="mega-menu-button relative text-white py-4 px-4 bg-celeste rounded-lg w-full h-16 transition-all duration-500 ease-in-out group hover:bg-verde before:absolute before:inset-0 before:bg-verde before:border before:rounded-lg before:transition-all before:duration-300 before:ease-linear before:origin-left before:scale-x-0 hover:before:scale-x-100">
+                  <div class="absolute left-2 top-1/2 w-12 h-12 -translate-y-1/2 bg-white p-2 rounded-2xl border border-transparent flex justify-center items-center">
+                    <Icon name="mdi:book-open-variant" class="inner-icon text-celeste text-3xl group-hover:text-verde" />
                   </div>
-                  <span class="absolute text-white top-1/2 -translate-y-1/2">Monografie</span>
+                  <span class="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Monografie</span>
                 </button>
               </div>
               <div class="p-2">
-                <button class="mega-menu-button relative text-white py-4 px-4 bg-celeste rounded-lg w-full h-16 transition-all duration-500 ease-in-out hover:bg-verde before:absolute before:inset-0 before:bg-verde before:border before:rounded-lg before:transition-width before:duration-300 before:ease-linear before:origin-left before:scale-x-0 hover:before:scale-x-100">
-                  <div class="absolute left-2 top-1/2 w-12 h-12 -translate-y-1/2 bg-white p-2 rounded-2xl border border-transparent">
-                    <Icon name="mdi:handshake" class="inner-icon text-celeste text-3xl" />
+                <button class="mega-menu-button relative text-white py-4 px-4 bg-celeste rounded-lg w-full h-16 transition-all duration-500 ease-in-out group hover:bg-verde before:absolute before:inset-0 before:bg-verde before:border before:rounded-lg before:transition-all before:duration-300 before:ease-linear before:origin-left before:scale-x-0 hover:before:scale-x-100">
+                  <div class="absolute left-2 top-1/2 w-12 h-12 -translate-y-1/2 bg-white p-2 rounded-2xl border border-transparent flex justify-center items-center">
+                    <Icon name="mdi:handshake" class="inner-icon text-celeste text-3xl group-hover:text-verde" />
                   </div>
-                  <span class="absolute text-white top-1/2 -translate-y-1/2">Collabora con noi</span>
+                  <span class="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Collabora con noi</span>
                 </button>
               </div>
             </div>
@@ -88,35 +88,13 @@
     </div>
 
     <!-- Mega menu Mailing List -->
-    <div v-if="mailingListMenuOpen" class="menu-container w-full bg-white p-6 rounded-bl-2xl rounded-br-2xl transition-all duration-500 ease-out">
-      <div class="container mx-auto px-4">
-        <div class="flex">
-          <!-- Left col -->
-          <div class="w-1/3">
-            <p class="text-gray-700 text-lg">
-              Amplia le tue conoscenze erboristiche, iscriviti alla <span class="text-blu">mailing list</span> di Wikiherbalist
-            </p>
-          </div>
-
-          <!-- Right col -->
-          <div class="w-2/3 flex justify-end items-center p-2">
-            <div class="w-2/3 h-14 flex items-center border border-celeste rounded-2xl overflow-hidden py-2 px-4">
-              <input
-                type="email"
-                placeholder="La tua email"
-                class="flex-grow focus:outline-none"
-              />
-              <Icon name="tabler:mail-filled" class="text-celeste text-5xl pr-4" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <MailingList v-if="mailingListMenuOpen" @close="mailingListMenuOpen = false" />
   </header>
 </template>
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
+import MailingList from './mailinglist.vue';
 
 const menuOpen = ref(false);
 const mailingListMenuOpen = ref(false);
@@ -150,14 +128,7 @@ onUnmounted(() => {
 <style scoped>
 .bar {
   background-color: #a2b4c0;
-  width: 6px;
-  height: 28px;
-  margin: 0 2px;
   transition: all 0.4s cubic-bezier(0.455, 0.03, 0.515, 0.955);
-}
-
-.bar:nth-child(2) {
-  height: 21px; 
 }
 
 .hamburger-menu.active .bar:nth-child(1) {
@@ -173,30 +144,5 @@ onUnmounted(() => {
 .hamburger-menu.active .bar:nth-child(3) {
   transform: translateX(2px) rotate(30deg);
   background-color: #036297;
-}
-
-.buttonMailing {
-  background-color: white;
-  color: #0DA6A4;
-  border: 1px solid #0DA6A4; 
-  border-radius: 8px;
-  padding: 5px 20px;
-}
-
-.buttonMailing:hover {
-  color: #036297;
-  border: 1px solid #036297;
-}
-
-nav a:hover {
-  color: #0DA6A4;
-}
-
-.mega-menu-button:hover .inner-icon {
-  color: #0DA6A4;
-}
-
-.mega-menu-button span {
-  left: 40%;
 }
 </style>
