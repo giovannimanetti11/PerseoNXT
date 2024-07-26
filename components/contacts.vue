a7e68a49d3eed974afe6ed864422be69eaba461a
giovannimanetti11 <giovanni@perseodesign.com>
2024-07-26 14:22:55 +0000

    feat(contacts): improve responsiveness for mobile devices

--- components/contacts.vue
+++ components/contacts.vue
@@ -1,8 +1,8 @@
 <template>
-    <section class="py-8 px-10 mt-6 w-11/12 mx-auto rounded-2xl section-contacts flex">
-        <div class="w-2/5 p-2">
+    <section class="py-8 px-10 mt-6 w-11/12 mx-auto rounded-2xl section-contacts flex flex-col md:flex-row">
+        <div class="w-full md:w-2/5 p-2">
             <h3 class="text-3xl font-bold mb-4">Contatti</h3>
-            <p class="mb-6 w-4/5">
+            <p class="mb-6 w-full md:w-4/5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit nunc et velit rhoncus, sed fermentum risus feugiat. Aliquam at turpis dictum, hendrerit metus eget, lobortis est.
             </p>
             <div class="flex mt-4 w-12 h-12">
@@ -14,7 +14,7 @@
                 </a>
             </div>
         </div>
-        <div class="w-3/5 p-2">
+        <div class="w-full md:w-3/5 p-2">
             <ContactForm />
         </div>
     </section>
