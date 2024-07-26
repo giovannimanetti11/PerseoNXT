2eaa7d5ca1df9bc64c8f2fb6f967cdbd004f8b29
giovannimanetti11 <giovanni@perseodesign.com>
2024-07-26 14:42:50 +0000

    feat(blog): enhance responsiveness and mobile layout

--- pages/blog/[...uri].vue
+++ pages/blog/[...uri].vue
@@ -1,47 +1,51 @@
 <template>
   <div id="post" v-if="post">
-    <!-- Main upper blogpost container -->
-    <section class="postBlog-info-section flex flex-row py-20 px-10 w-11/12 mx-auto rounded-2xl print:py-2 print:px-0 print:w-full">
-      <!-- Container for main blogpost information -->
-      <div class="mt-40 container mx-auto w-3/5 px-4 print:mt-8 print:px-0">
-        <BlogInfo 
-          :title="post.title"
-          :publishDate="post.date"
-          :authorName="post.authorName"
-          :readingTime="readingTime"
+    <section class="postBlog-info-section flex flex-col py-10 md:py-20 px-4 md:px-10 w-11/12 mx-auto rounded-2xl print:py-2 print:px-0 print:w-full">
+      <!-- Container for featured image (mobile) -->
+      <div class="md:hidden w-full mt-8 md:mt-0 mb-8">
+        <NuxtImg 
+          v-if="post.featuredImage" 
+          class="m-auto h-60 w-auto border rounded-2xl transition-all duration-300 ease-in-out shadow-lg" 
+          :src="post.featuredImage.node.sourceUrl" 
+          :alt="post.featuredImage.node.altText" 
         />
       </div>
-      <!-- Container for featured image -->
-      <div class="flex flex-col w-2/5">
-        <NuxtImg v-if="post.featuredImage" class="m-auto h-60 w-auto border rounded-2xl transition-all duration-300 ease-in-out shadow-lg mb-4" :src="post.featuredImage.node.sourceUrl" :alt="post.featuredImage.node.altText" />
+
+      <div class="flex flex-col md:flex-row">
+        <!-- Container for main post information -->
+        <div class="w-full md:w-3/5 lg:w-2/5 md:mt-40 container mx-auto px-2 print:mt-8 print:px-0 order-2 md:order-1">
+          <BlogInfo 
+            :title="post.title"
+            :publishDate="post.date"
+            :authorName="post.authorName"
+            :readingTime="readingTime"
+          />
+        </div>
+
+        <!-- Container for featured image (desktop) -->
+        <div class="hidden md:flex w-full md:w-2/5 lg:w-3/5 flex-col order-1 md:order-2 md:justify-center md:items-end">
+          <NuxtImg 
+            v-if="post.featuredImage" 
+            class="m-auto md:m-0 h-60 w-auto border rounded-2xl transition-all duration-300 ease-in-out shadow-lg mb-4" 
+            :src="post.featuredImage.node.sourceUrl" 
+            :alt="post.featuredImage.node.altText" 
+          />
+        </div>
       </div>
     </section>
-    <!-- Index section -->
-    <section class="postBlog-index-section flex flex-col py-20 px-10 w-11/12 mx-auto rounded-2xl mt-4 print:py-2 print:px-0 print:w-full">
-      <div class="font-bold text-2xl">
-        <icon name="ic:twotone-list" class="text-3xl text-black rounded-full mr-2" /> Indice
-      </div>
-      <ul class="mt-8 flex flex-wrap justify-items-start gap-4 print:gap-0 print:mt-2">
-        <li v-for="(heading, index) in headings" :key="index" class="text-center py-4 px-4 bg-verde text-white rounded-xl m-1 text-sm hover:bg-celeste cursor-pointer w-1/5 flex-grow-0 flex-shrink-0">
-          <a :href="'#section' + (index + 1)" class="flex items-center group" @click.prevent="smoothScroll('#section' + (index + 1))">
-            <div class="circle flex items-center justify-center w-8 h-8 bg-white text-verde rounded-full mr-2 text-lg font-bold group-hover:text-celeste">{{ index + 1 }}</div>
-            {{ heading }}
-          </a>
-        </li>
-      </ul>
-    </section>
+
 
     <!-- Start of content sections -->
     <section v-for="(section, index) in sections"
-             :class="['blogpost-content-section flex flex-col py-20 px-10 w-11/12 mx-auto rounded-2xl mt-4 print:py-2 print:px-0 print:w-full', section.className]"
-             :id="'section' + (index + 1)"
-             :key="section.heading">
-      <div class="flex items-center" v-if="section.heading !== 'Riferimenti'">
-        <div class="circle flex items-center justify-center w-12 h-12 mb-4 mr-2 bg-blu text-white rounded-full text-lg font-bold print:mb-0 print:mr-0.5">{{ index + 1 }}</div>
-        <h3>{{ section.heading }}</h3>
-      </div>
-      <h3 v-else class="mb-4">{{ section.heading }}</h3>
-      <div v-html="sanitizedContent(section.content)" class="mt-4"></div>
+        :class="['term-section flex flex-col py-10 md:py-20 px-4 md:px-10 w-11/12 mx-auto rounded-2xl mt-4 print:py-2 print:px-0 print:w-full', section.className]"
+        :id="'section' + (index + 1)"
+        :key="section.heading">
+    <div class="flex items-start" v-if="section.heading !== 'Riferimenti'">
+    <div class="circle flex-shrink-0 flex items-center justify-center w-8 h-8 md:w-12 md:h-12 mb-2 md:mb-4 mr-2 bg-blu text-white rounded-full text-base md:text-lg font-bold print:mb-0 print:mr-0.5">{{ index + 1 }}</div>
+    <h3 class="text-xl md:text-2xl mt-1 md:mt-0 flex-grow">{{ section.heading }}</h3>
+    </div>
+    <h3 v-else class="text-xl md:text-2xl mb-4">{{ section.heading }}</h3>
+    <div v-html="sanitizedContent(section.content)" class="mt-4"></div>
     </section>
   </div>
   <div v-else class="flex flex-row py-20 px-10 w-11/12 mx-auto rounded-2xl">
