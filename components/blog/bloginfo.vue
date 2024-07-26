398993b1d4e7f1b16d1bb04179095af1e3f6738b
giovannimanetti11 <giovanni@perseodesign.com>
2024-07-26 14:20:34 +0000

    refactor(bloginfo): remove text-to-speech feature and adjust icon sizes

--- components/blog/bloginfo.vue
+++ components/blog/bloginfo.vue
@@ -2,22 +2,19 @@
   <div class="flex flex-col">
     <div class="flex flex-row">
       <h1 class="text-3xl font-bold">{{ title }}</h1>
-      <button @click="speakTitle" class="flex text-5xl ml-4 mb-2 shadow text-white bg-blu rounded-full w-12 h-12 justify-center items-center hover:bg-white hover:text-blu print:hidden">
-        <icon name="f7:speaker-1-fill" class="text-4xl" />
-      </button>
     </div>
     <p class="text-xl italic text-gray-700">{{ nomeScientifico }}</p>
     <div class="postinfo-buttons flex mt-4 print:hidden">
       <button @click="printPost" class="group flex items-center mr-4 py-2 px-4 bg-white shadow text-blu border rounded-lg hover:bg-blu hover:text-white">
-        <icon name="mingcute:print-fill" class="text-3xl group-hover:text-white" />
+        <icon name="mingcute:print-fill" class="text-xl md:text-3xl group-hover:text-white" />
         <span class="ml-2 text-base">Stampa</span>
       </button>
       <button class="group flex items-center mr-4 py-2 px-4 bg-white shadow text-blu border rounded-lg hover:bg-blu hover:text-white">
-        <icon name="ic:round-share" class="text-3xl group-hover:text-white" />
+        <icon name="ic:round-share" class="text-xl md:text-3xl group-hover:text-white" />
         <span class="ml-2 text-base">Condividi</span>
       </button>
       <button class="group flex items-center py-2 px-4 bg-white shadow text-blu border rounded-lg hover:bg-blu hover:text-white">
-        <icon name="bi:quote" class="text-3xl group-hover:text-white" />
+        <icon name="bi:quote" class="text-xl md:text-3xl group-hover:text-white" />
         <span class="ml-2 text-base">Cita</span>
       </button>
     </div>
@@ -66,13 +63,5 @@ const getFormattedDate = dateStr => {
 const formattedPublishDate = computed(() => getFormattedDate(props.publishDate));
 const formattedUpdateDate = computed(() => getFormattedDate(props.updateDate));
 
-const speakTitle = () => {
-  if ('speechSynthesis' in window) {
-    const utterance = new SpeechSynthesisUtterance(props.title);
-    utterance.lang = 'it-IT';
-    window.speechSynthesis.speak(utterance);
-  } else {
-    alert('La sintesi vocale non è supportata in questo browser.');
-  }
-};
+
 </script>
