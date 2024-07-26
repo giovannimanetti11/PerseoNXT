c7bc0610c6350d9da9e43c7fd0bdea1299b002f9
giovannimanetti11 <giovanni@perseodesign.com>
2024-07-26 14:10:17 +0000

    feat(home): add responsive design to recent blog posts section

--- components/section-blog.vue
+++ components/section-blog.vue
@@ -1,74 +1,70 @@
 <template>
-  <section class="py-10 mt-6 w-11/12 mx-auto rounded-2xl section-blog flex"> 
-      <!-- Left column -->
-      <div class="w-2/5 p-8 ml-6">
-          <h3 class="text-3xl font-bold mb-4">Blog</h3>
-          <p class="mb-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit nunc et velit rhoncus, sed fermentum risus feugiat. Aliquam at turpis dictum, hendrerit metus eget, lobortis est.
-          </p>
-          <button class="bg-transparent text-blu py-2 px-4 border border-blu rounded-xl hover:text-verde hover:border-verde">
-          Esplora gli articoli
-          </button>
-      </div>
-      <!-- Right column -->
-      <div class="w-3/5 flex flex-wrap justify-around">
-          <div class="w-full flex flex-wrap justify-around">
-            <div v-for="(post, index) in posts" :key="post.uri" @click="toggleActive(post.uri)"
-              :class="['blog-card w-7/12 flex flex-row rounded-2xl shadow transition-all m-4 cursor-pointer', post.uri === activePost ? 'active bg-celeste text-white z-10' : 'bg-white shadow text-black hover:bg-celeste hover:text-white']"
-              :style="{ transform: getTransformation(post, index) }">
-                  <div class="px-6 py-4 flex flex-col justify-between items-start h-full w-2/4">
-                      <div class="font-bold text-xl mb-auto">{{ post.title }}</div>
-                      <div class="text-gray-500 blog-details">{{ post.authorName }}</div>
-                      <div class="text-gray-500 blog-details">{{ post.date }}</div>
-                  </div>
-                  <div class="flex-col w-2/4 m-auto text-center">
-                      <NuxtImg :src="post.featuredImage" :alt="post.altText" class="rounded-2xl w-11/12 mt-4 m-auto h-auto max-h-32 object-cover" />
-                      <nuxt-link :to="post.uri">
-                          <button class="blog-card-button py-4 bg-verde text-white w-11/12 rounded-xl mt-4 mb-4">
-                              Leggi di più  →
-                          </button>
-                      </nuxt-link>
-                  </div>
-              </div>
+  <section class="py-10 mt-6 w-11/12 mx-auto rounded-2xl section-blog flex flex-col xl:flex-row"> 
+    <!-- Left column -->
+    <div class="w-full xl:w-2/5 p-8 xl:ml-6">
+      <h3 class="text-3xl font-bold mb-4">Blog</h3>
+      <p class="mb-6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit nunc et velit rhoncus, sed fermentum risus feugiat. Aliquam at turpis dictum, hendrerit metus eget, lobortis est.
+      </p>
+      <button class="bg-transparent text-blu py-2 px-4 border border-blu rounded-xl hover:text-verde hover:border-verde">
+        Esplora gli articoli
+      </button>
+    </div>
+    <!-- Right column -->
+    <div class="w-full xl:w-3/5">
+      <div class="w-full flex flex-col xl:flex-wrap xl:justify-around">
+        <div v-for="(post, index) in posts" :key="post.uri" @click="toggleActive(post.uri)"
+          :class="['blog-card w-11/12 xl:w-7/12 flex flex-col xl:flex-row rounded-2xl shadow transition-all m-auto my-2 xl:m-4 cursor-pointer', 
+            post.uri === activePost ? 'active bg-celeste text-white' : 'bg-white shadow text-black hover:bg-celeste hover:text-white']"
+          :style="{ transform: getTransformation(post, index) }">
+          <div class="px-6 py-4 flex flex-col justify-between items-start h-full w-full xl:w-2/4">
+            <div class="font-bold text-xl mb-auto">{{ post.title }}</div>
+            <div class="text-gray-500 blog-details">{{ post.authorName }}</div>
+            <div class="text-gray-500 blog-details">{{ post.date }}</div>
           </div>
+          <div class="flex-col w-full xl:w-2/4 m-auto text-center">
+            <NuxtImg :src="post.featuredImage" :alt="post.altText" class="rounded-2xl w-11/12 mt-4 m-auto h-auto max-h-32 object-cover" />
+            <nuxt-link :to="post.uri">
+              <button class="blog-card-button py-4 bg-verde text-white w-11/12 rounded-xl mt-4 mb-4">
+                Leggi di più  →
+              </button>
+            </nuxt-link>
+          </div>
+        </div>
       </div>
+    </div>
   </section>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue';
+import { ref, watch, computed } from 'vue';
 import { useQuery } from '@vue/apollo-composable';
 import gql from 'graphql-tag';
 
-// Define ref for posts
 const posts = ref([]);
-
 const activePost = ref(null);
 
-// GraphQL query for blog posts
 const BLOG_POSTS_QUERY = gql`
-query BlogPosts {
-  blogPosts(first: 3, where: {orderby: {field: DATE, order: DESC}}) {
-    nodes {
-      authorName
-      featuredImage {
-        node {
-          altText
-          sourceUrl
+  query BlogPosts {
+    blogPosts(first: 3, where: {orderby: {field: DATE, order: DESC}}) {
+      nodes {
+        authorName
+        featuredImage {
+          node {
+            altText
+            sourceUrl
+          }
         }
+        title
+        uri
+        date
       }
-      title
-      uri
-      date
     }
   }
-}
 `;
 
-// Use Apollo Composable to fetch blog posts
 const { result } = useQuery(BLOG_POSTS_QUERY);
 
-// Watch for changes in result
 watch(result, (newValue) => {
   if (newValue?.blogPosts?.nodes) {
     posts.value = newValue.blogPosts.nodes.map(post => ({
@@ -78,16 +74,22 @@ watch(result, (newValue) => {
       title: post.title,
       uri: post.uri,
       date: new Date(post.date).toLocaleDateString('it-IT', {
-          day: 'numeric',
-          month: 'long',
-          year: 'numeric'
-        })
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+      })
     }));
     activePost.value = posts.value[0]?.uri;
   }
 });
 
-// Define ref for active transformations
+const isXLScreen = computed(() => {
+  if (typeof window !== 'undefined') {
+    return window.innerWidth >= 1280; // Tailwind's XL breakpoint
+  }
+  return false;
+});
+
 const transformations = ref({
   first: 'translateX(40px)',
   second: 'translate(-80px, -100px)',
@@ -95,6 +97,8 @@ const transformations = ref({
 });
 
 const toggleActive = (uri) => {
+  if (!isXLScreen.value) return;
+  
   if (activePost.value === uri) {
     activePost.value = null;
     transformations.value = {
@@ -114,6 +118,8 @@ const toggleActive = (uri) => {
 };
 
 const getTransformation = (post, index) => {
+  if (!isXLScreen.value) return '';
+  
   switch (index) {
     case 0: return transformations.value.first;
     case 1: return transformations.value.second;
@@ -124,18 +130,22 @@ const getTransformation = (post, index) => {
 </script>
 
 <style scoped>
+.section-blog {
+  background-image: linear-gradient(180deg, rgba(245,245,245,1) 0%, rgba(224,237,253,1) 82%);
+}
+
+.blog-card:hover .blog-details, .blog-card.active .blog-details {
+  color: #fff;
+}
+
+.blog-card:hover .blog-card-button:hover, .blog-card:hover .blog-card-button, .blog-card.active .blog-card-button {
+  background-color: #fff;
+  color: #5E9EF4;
+}
+
+@media (min-width: 1280px) {
   .section-blog {
-    background-image: linear-gradient(180deg, rgba(245,245,245,1) 0%, rgba(224,237,253,1) 82%);
     max-height: 625px;
-  }
-
-  .blog-card:hover .blog-details, .blog-card.active .blog-details {
-    color: #fff;
-  }
-
-  .blog-card:hover .blog-card-button:hover, .blog-card:hover .blog-card-button, .blog-card.active .blog-card-button {
-    background-color: #fff;
-    color: #5E9EF4;
   }
 
   .blog-card:first-child {
@@ -159,4 +169,5 @@ const getTransformation = (post, index) => {
     z-index: 10;
     animation: bounce 0.5s forwards;
   }
+}
 </style>
