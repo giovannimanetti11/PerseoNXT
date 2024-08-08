639184599d34ea28da6f68379161240aac761e24
giovannimanetti11 <giovanni@perseodesign.com>
2024-08-08 09:46:00 +0000

    feat: add aria-labels for better accessibility

--- components/contactform.vue
+++ components/contactform.vue
@@ -5,16 +5,17 @@
     </div>
     <div class="flex flex-col md:flex-row md:space-x-4">
       <div class="flex flex-col w-full md:w-1/2 space-y-4">
-        <input type="text" v-model="form.nome" placeholder="Nome" class="w-full placeholder-gray-300 border p-2 rounded-xl border-celeste focus:outline-none focus:ring-1 focus:ring-celeste">
-        <input type="text" v-model="form.cognome" placeholder="Cognome" class="w-full placeholder-gray-300 border p-2 rounded-xl border-celeste focus:outline-none focus:ring-1 focus:ring-celeste">
-        <input type="email" v-model="form.email" placeholder="Mail" class="w-full placeholder-gray-300 border p-2 rounded-xl border-celeste focus:outline-none focus:ring-1 focus:ring-celeste" :class="{'input-error': v$.email.$error}" required>
+        <input type="text" v-model="form.nome" placeholder="Nome" aria-label="Nome" class="w-full placeholder-gray-300 border p-2 rounded-xl border-celeste focus:outline-none focus:ring-1 focus:ring-celeste">
+        <input type="text" v-model="form.cognome" placeholder="Cognome" aria-label="Cognome" class="w-full placeholder-gray-300 border p-2 rounded-xl border-celeste focus:outline-none focus:ring-1 focus:ring-celeste">
+        <input type="email" v-model="form.email" placeholder="Email" aria-label="Email" class="w-full placeholder-gray-300 border p-2 rounded-xl border-celeste focus:outline-none focus:ring-1 focus:ring-celeste" :class="{'input-error': v$.email.$error}" required>
         <div v-if="v$.email.$error" class="text-red-500 text-sm">Inserisci un indirizzo email valido</div>
-        <input type="tel" v-model="form.telefono" placeholder="Telefono" class="w-full placeholder-gray-300 border p-2 rounded-xl border-celeste focus:outline-none focus:ring-1 focus:ring-celeste">
+        <input type="tel" v-model="form.telefono" placeholder="Telefono" aria-label="Telefono" class="w-full placeholder-gray-300 border p-2 rounded-xl border-celeste focus:outline-none focus:ring-1 focus:ring-celeste">
       </div>
       <div class="w-full md:w-1/2 mt-4 md:mt-0">
         <textarea 
           v-model="form.richiesta" 
           placeholder="La tua richiesta" 
+          aria-label="La tua richiesta" 
           class="w-full h-64 md:h-full placeholder-gray-300 border p-2 rounded-xl border-celeste focus:outline-none focus:ring-1 focus:ring-celeste resize-none" 
           :class="{'input-error': v$.richiesta.$error}" 
           required
@@ -30,6 +31,7 @@
     </div>
   </div>
 </template>
+
 
 <script setup>
 import { reactive } from 'vue';
@@ -62,12 +64,13 @@ const message = reactive({
   type: ''
 });
 
+// Function to handle form submission
 const sendEmail = async () => {
-  if (form.honeypot) return;
+  if (form.honeypot) return; // Prevent spam
   v$.value.$validate();
   if (!v$.value.$error) {
     try {
-      form.recaptchaToken = await $recaptcha.execute('submit');
+      form.recaptchaToken = await $recaptcha.execute('submit'); // Recaptcha for additional security
       console.log('Recaptcha token:', form.recaptchaToken);
 
       const response = await fetch('/api/sendEmail', {
@@ -81,12 +84,12 @@ const sendEmail = async () => {
 
       if (!response.ok) {
         const errorDetails = await response.text();
-        throw new Error(`La risposta del server non è OK: ${errorDetails}`);
+        throw new Error(`Server response not OK: ${errorDetails}`);
       }
 
       const data = await response.json();
       if (data.success) {
-        message.header = 'Inviato!';
+        message.header = 'Sent!';
         message.content = data.message;
         message.type = 'text-green-800 bg-green-50 dark:text-green-400';
       } else {
@@ -94,23 +97,29 @@ const sendEmail = async () => {
       }
     } catch (error) {
       console.error('Error sending email', error);
-      message.header = 'Errore!';
-      message.content = 'Si è verificato un errore nell\'invio della richiesta: ' + error.message;
+      message.header = 'Error!';
+      message.content = `Error submitting request: ${error.message}`;
       message.type = 'text-red-800 bg-red-50 dark:text-red-400';
     }
   } else {
-    if (v$.value.email.$error) {
-      message.header = 'Errore!';
-      message.content = 'Inserisci un indirizzo email valido.';
-      message.type = 'text-red-800 bg-red-50 dark:text-red-400';
-    } else if (v$.value.richiesta.$error) {
-      message.header = 'Errore!';
-      message.content = 'Scrivi un messaggio.';
-      message.type = 'text-red-800 bg-red-50 dark:text-red-400';
-    }
+    updateMessageForValidationErrors(); // Handle validation errors
+  }
+};
+
+// Update message based on validation errors
+const updateMessageForValidationErrors = () => {
+  if (v$.value.email.$error) {
+    message.header = 'Error!';
+    message.content = 'Please enter a valid email address.';
+    message.type = 'text-red-800 bg-red-50 dark:text-red-400';
+  } else if (v$.value.richiesta.$error) {
+    message.header = 'Error!';
+    message.content = 'Please write a message.';
+    message.type = 'text-red-800 bg-red-50 dark:text-red-400';
   }
 };
 </script>
+
 
 <style scoped>
 .input-error {
