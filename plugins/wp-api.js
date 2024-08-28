251025ac6929b4e22403364a965dedbffa045921
giovannimanetti11 <giovanni@perseodesign.com>
2024-08-28 11:02:03 +0000

    refactor: standardize apiConfig import and update Apollo cache policies

--- plugins/wp-api.js
+++ plugins/wp-api.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core';
 import { provideApolloClient } from '@vue/apollo-composable';
 import { defineNuxtPlugin } from '#app';
-import { apiConfig } from '~/config.js';
+import { apiConfig } from '@config';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const httpLink = createHttpLink({
@@ -11,7 +11,24 @@ export default defineNuxtPlugin((nuxtApp) => {
     }
   });
 
-  const cache = new InMemoryCache();
+  const cache = new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          posts: {
+            merge(existing, incoming) {
+              return incoming;
+            }
+          },
+          glossaryTerms: {
+            merge(existing, incoming) {
+              return incoming;
+            }
+          }
+        }
+      }
+    }
+  });
 
   const apolloClient = new ApolloClient({
     link: httpLink,
