b36dc3c6cba59c2f4cc7547ac0ad74896f973f22
giovannimanetti11 <giovanni@perseodesign.com>
2024-09-02 09:49:10 +0000

    refactor(section-contacts): add section ID and disable social links

--- components/contacts.vue
+++ components/contacts.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Section for contact information and social media links -->
-    <section class="py-8 px-4 md:px-10 mt-6 w-11/12 mx-auto rounded-2xl section-contacts flex flex-col md:flex-row">
+    <section id="section-contacts" class="py-8 px-4 md:px-10 mt-6 w-11/12 mx-auto rounded-2xl section-contacts flex flex-col md:flex-row">
         <!-- Contact information area -->
         <div class="w-full md:w-2/5 p-2">
             <h3 class="text-3xl font-bold mb-4">Contatti</h3>
@@ -10,10 +10,10 @@
             </p>
             <!-- Social media icons -->
             <div class="flex mt-4 w-12 h-12">
-                <a href="#" aria-label="Facebook" class="bg-white text-blu p-2 rounded-2xl transition-colors duration-300 hover:bg-verde hover:text-white mr-2">
+                <a href="#" aria-label="Facebook" class="pointer-events-none bg-white text-blu p-2 rounded-2xl transition-colors duration-300 hover:bg-verde hover:text-white mr-2">
                     <Icon name="uil:facebook-f" class="text-3xl" />
                 </a>
-                <a href="#" aria-label="Instagram" class="bg-white text-blu p-2 rounded-2xl transition-colors duration-300 hover:bg-verde hover:text-white">
+                <a href="#" aria-label="Instagram" class="pointer-events-none bg-white text-blu p-2 rounded-2xl transition-colors duration-300 hover:bg-verde hover:text-white">
                     <Icon name="uil:instagram" class="text-3xl" />
                 </a>
             </div>
