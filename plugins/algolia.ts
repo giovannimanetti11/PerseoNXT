7b24548ab34a3f5a16dfbed611550b01d8dcd26a
giovannimanetti11 <giovanni@perseodesign.com>
2024-10-28 16:11:18 +0000

    refactor(algolia): rename from .js to .ts

--- plugins/algolia.ts
+++ plugins/algolia.ts
@@ -2,7 +2,8 @@ import algoliasearch from 'algoliasearch';
 import { defineNuxtPlugin } from '#app';
 import { apiConfig } from '~/config';
 
-export default defineNuxtPlugin(nuxtApp => {
+// Initialize and provide the Algolia client to the Nuxt application.
+export default defineNuxtPlugin((nuxtApp) => {
   const algoliaClient = algoliasearch(apiConfig.algoliaAppId, apiConfig.algoliaAdminAPIKey);
   nuxtApp.provide('algoliaClient', algoliaClient);
 });
