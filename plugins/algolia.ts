54fa7ce28e0f1c0c84b85fa8d518895d6901b45c
giovannimanetti11 <giovanni@perseodesign.com>
2025-10-20 09:20:20 +0200

    chore: update dependencies and configuration with security patches

--- plugins/algolia.ts
+++
@@ -1,9 +0,0 @@
-import algoliasearch from 'algoliasearch';
-import { defineNuxtPlugin } from '#app';
-import { apiConfig } from '~/config';
-
-// Initialize and provide the Algolia client to the Nuxt application.
-export default defineNuxtPlugin((nuxtApp) => {
-  const algoliaClient = algoliasearch(apiConfig.algoliaAppId, apiConfig.algoliaAdminAPIKey);
-  nuxtApp.provide('algoliaClient', algoliaClient);
-});
